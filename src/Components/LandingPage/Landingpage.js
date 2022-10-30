7a701a9b33578e7308c470ac54728cb0bbfb7162
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-30 23:08:15 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Landingpage.js
+++ src/Components/LandingPage/Landingpage.js
@@ -5,7 +5,7 @@ function Landingpage() {
   return (
     <div className='Landingpage'>
       <div className='nav'>
-        
+        <img src='https://www.logo.wine/a/logo/LinkedIn/LinkedIn-Logo.wine.svg' alt='linkedIn logo'/>
       </div>
     </div>
   )
