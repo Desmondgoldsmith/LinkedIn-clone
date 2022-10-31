78e09698a1002060b3918941a9435e7771f7399d
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-31 02:33:42 +0000

    Working on the LandingPage 💥

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Landingpage from './Components/LandingPage/Landingpage';
+import Topics from './Components/LandingPage/Topics';
 import Welcome from './Components/LandingPage/Welcome';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Landingpage />
       <Welcome />
+      <Topics />
     </div>
   );
 }
