46d4be162969b712ce4822ee2f8fd6d3f43fe828
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-31 22:12:15 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Jobs.js
+++ src/Components/LandingPage/Jobs.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './jobs.css'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 function Jobs() {
   return (
     <div className = 'Jobs'>
-      
+      <div className ='title'>
+      <h2>Find the right job or internship for you</h2>
+      </div>
+     <div className = 'jobs_types'>
+      <p>SUGGESTED SEARCHES</p>
+      <button>Engineering</button>
+      <button>Business Development</button>
+      <button>Finance</button>
+      <button>Administrative Assistant</button>
+      <button>Research Associate</button>
+      <button>Retail Associate</button>
+      <button>Customer Services</button>
+      <button>Operations</button>
+      <button>Information Technology</button>
+      <button>Marketing</button>
+      <button>Human Resource</button>
+      <p>show more <KeyboardArrowDownIcon/> </p>
+    </div>
     </div>
   )
 }
