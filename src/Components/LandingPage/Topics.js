ec4b17967e459b3ede87d48af736fb76789951df
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-31 03:20:14 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Topics.js
+++ src/Components/LandingPage/Topics.js
@@ -7,7 +7,8 @@ function Topics() {
      <div className='topics_container'>
         <p className='main'>Explore topics you are interested in</p>
      <p className='second'>CONTENT TOPICS</p>
-     <button className='var_topics'>see All Topics</button>
+     <div className='buttons'>
+        <button className='var_topics'>see All Topics</button>
      <button className='var_topics'>Workplace</button>
      <button className='var_topics'>Job Search</button>
      <button className='var_topics'>Careers</button>
@@ -15,6 +16,8 @@ function Topics() {
      <button className='var_topics'>Salary and Compensation</button>
      <button className='var_topics'>Internships</button>
      <button className='var_topics'>Employee Benefits</button>
+     
+     </div>
      </div>
      </div>
   )
