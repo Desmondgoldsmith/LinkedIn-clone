61802f7d30fa579598046c9eb908f276b3363a78
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-31 03:43:40 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Careerdetails.js
+++ src/Components/LandingPage/Careerdetails.js
@@ -3,8 +3,12 @@ import React from 'react'
 function Careerdetails({image,title,logo,subtitle,details}) {
   return (
     <div className='details'>
-      <div className = 'indv_detail'>
+      <div className = 'image'>
         <img  src = {image} alt = {title} />
+      </div>
+      <div className='indv_detail'>
+         <h4>{title}</h4>
+         <p>{logo}{subtitle}</p>
       </div>
     </div>
   )
