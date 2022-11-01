92b1278ec8fd7f4829556963603ab06ab11b3ac1
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-01 02:20:15 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Testimonals.js
+++ src/Components/LandingPage/Testimonals.js
@@ -7,21 +7,10 @@ function Testimonals() {
   return (
     <div className='Testimonials'>
        <Testdata 
-        image = ""
-        title = ""
-        description = ""
+        image = "https://static-exp1.licdn.com/aero-v1/sc/h/dbvmk0tsk0o0hd59fi64z3own"
+        title = "Let the right people know you’re open to work"
+        description = "With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities."
        />
-      <div className='text'>
-     <h2>Let the right people know you’re open to work</h2>
-        <p>With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities.</p>
-    </div>
- <div className='test_image'>
- <div className='nav'>
-       <p><ChevronLeftIcon/> previous</p>
-       <p>next<ChevronRightIcon/></p>
-     </div> 
-        <img src='https://static-exp1.licdn.com/aero-v1/sc/h/dbvmk0tsk0o0hd59fi64z3own' alt = 'testimonial_image'/>
-      </div>
       </div>
   )
 }
