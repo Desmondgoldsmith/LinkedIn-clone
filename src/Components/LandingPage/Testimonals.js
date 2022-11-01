2a8a2f2f43ac87a7e62d15c6c5fb1856a338e8e7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-01 01:54:26 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Testimonals.js
+++ src/Components/LandingPage/Testimonals.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './testimonials.css'
-
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 function Testimonals() {
   return (
     <div className='Testimonials'>
       <div className='content'>
+      <div className='nav'>
+       <p><ChevronLeftIcon/> previous</p>
+       <p>next<ChevronRightIcon/></p>
+     </div>
       <div className='text'>
         <h2>Let the right people know you’re open to work</h2>
         <p>With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities.</p>
