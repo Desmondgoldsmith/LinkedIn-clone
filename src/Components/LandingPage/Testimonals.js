2c29275d9a4310d45594a66c24f7d57a46051985
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-01 01:25:52 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Testimonals.js
+++ src/Components/LandingPage/Testimonals.js
@@ -4,12 +4,14 @@ import './testimonials.css'
 function Testimonals() {
   return (
     <div className='Testimonials'>
+      <div className='content'>
       <div className='text'>
         <h2>Let the right people know you’re open to work</h2>
         <p>With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities.</p>
     </div>
  <div className='test_image'>
         <img src='https://static-exp1.licdn.com/aero-v1/sc/h/dbvmk0tsk0o0hd59fi64z3own' alt = 'testimonial_image'/>
+      </div>
       </div>
       
     </div>
