13d2ab68b6d6ad89759ffa69ac1ae82f32327ed7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-01 02:28:08 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Testdata.js
+++ src/Components/LandingPage/Testdata.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import './testimonials.css'
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 function Testdata({image,title,description}) {
   return (
     <div className='content'>
-    <div className='nav'>
-       <p><ChevronLeftIcon/> previous</p>
-       <p>next<ChevronRightIcon/></p>
-     </div> 
       <div className = 'wrapper'>
        <div className='text'>
      <h2>{title}</h2>
--- src/Components/LandingPage/Testimonals.js
+++ src/Components/LandingPage/Testimonals.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './testimonials.css'
 import Testdata from './Testdata';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+
+
 function Testimonals() {
   return (
     <div className='Testimonials'>
+    <div className='nav'>
+       <p><ChevronLeftIcon/> previous</p>
+       <p>next<ChevronRightIcon/></p>
+     </div> 
        <Testdata 
         image = "https://static-exp1.licdn.com/aero-v1/sc/h/dbvmk0tsk0o0hd59fi64z3own"
         title = "Let the right people know you’re open to work"
