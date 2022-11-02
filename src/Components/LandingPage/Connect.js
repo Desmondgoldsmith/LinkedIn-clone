15ce6b4010da0dabb2dd35be441a01bae609a86f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-02 02:37:20 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Connect.js
+++ src/Components/LandingPage/Connect.js
@@ -5,10 +5,14 @@ function Connect() {
   return (
     <div className = 'Connect'>
       <div className = 'Connect_p'>
-
+         <img src = '' alt = 'connect'/>
+         <h2>Connect with people who can help</h2>
+         <button>Find people you know</button>
       </div>
       <div className = 'learn'>
-        
+      <img src = '' alt = 'connect'/>
+         <h2>Connect with people who can help</h2>
+         <button>Find people you know</button>
       </div>
     </div>
   )
