548b6c1d93cfa9683b8b1e7cf8d8992b2a18b58e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-02 02:34:59 +0000

    Working on the LandingPage 💥

---
+++ src/Components/LandingPage/Connect.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './connect.js'
+
+function Connect() {
+  return (
+    <div className = 'Connect'>
+      <div className = 'Connect_p'>
+
+      </div>
+      <div className = 'learn'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Connect
