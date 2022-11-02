7902998f827842170bc28ac68be51034f265b38f
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-02 04:22:59 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Testimonals.js
+++ src/Components/LandingPage/Testimonals.js
@@ -22,9 +22,15 @@ function Testimonals() {
         description = "With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities."
        />
         <Testdata 
-        image = "https://static-exp1.licdn.com/aero-v1/sc/h/dbvmk0tsk0o0hd59fi64z3own"
-        title = "Let the right people know you’re open to work"
-        description = "With the Open To Work feature, you can privately tell recruiters or publicly share with the LinkedIn community that you are looking for new job opportunities."
+        image = "https://static-exp1.licdn.com/aero-v1/sc/h/2r8kd5zqpi905lkzsshdlvvn5"
+        title = "Conversations today could lead to opportunity tomorrow"
+        description = "Sending messages to people you know is a great way to strengthen relationships as you take the next step in your career."
+       />
+
+        <Testdata 
+        image = "https://static-exp1.licdn.com/aero-v1/sc/h/ann24vsq7r0ux3vipqa1n90gg"
+        title = "Stay up to date on your industry"
+        description = "From live videos, to stories, to newsletters and more, LinkedIn is full of ways to stay up to date on the latest discussions in your industry."
        />
 
 </div>
