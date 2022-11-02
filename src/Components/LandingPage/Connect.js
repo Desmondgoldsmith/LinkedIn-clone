c9c71d8de8832bcd4c201460903c654ac85c66b9
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-02 02:39:33 +0000

    Working on the LandingPage 💥

--- src/Components/LandingPage/Connect.js
+++ src/Components/LandingPage/Connect.js
@@ -5,13 +5,13 @@ function Connect() {
   return (
     <div className = 'Connect'>
       <div className = 'Connect_p'>
-         <img src = '' alt = 'connect'/>
+         <img src = 'https://static-exp1.licdn.com/aero-v1/sc/h/b1fxwht7hdbeusleja7ciftsj' alt = 'connect'/>
          <h2>Connect with people who can help</h2>
          <button>Find people you know</button>
       </div>
       <div className = 'learn'>
-      <img src = '' alt = 'connect'/>
-         <h2>Connect with people who can help</h2>
+      <img src = 'https://static-exp1.licdn.com/aero-v1/sc/h/dkfub4sc7jgzg3o31flfr91rv' alt = 'connect'/>
+         <h2>Learn the skills you need to succeed</h2>
          <button>Find people you know</button>
       </div>
     </div>
