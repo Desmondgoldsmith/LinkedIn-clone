ca3f4e875a86092d5bc9b6d873617083e70ed348
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-03 13:48:19 +0000

    Working on the LandingPage 💥

---
+++ src/Components/LandingPage/Footersection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footersection.css'
+
+function Footersection(){
+    return (
+        <div className="footer-section">
+         <h1>Footer Section</h1>
+        </div>
+    )
+}
+
+export default Footersection
