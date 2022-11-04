9e5e70e2a635575bfb53b2cba02bda2122ad4c57
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-04 17:01:54 +0000

    Done With the LAnding Page 😍

--- src/Components/Main Page/Navbar.js
+++ src/Components/Main Page/Navbar.js
@@ -4,13 +4,14 @@ import HomeIcon from '@mui/icons-material/Home';
 import GroupIcon from '@mui/icons-material/Group';
 import WorkIcon from '@mui/icons-material/Work';
 import TextsmsIcon from '@mui/icons-material/Textsms';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Navbar() {
   return (
     <div className='Navbar'>
       <div className = 'navbar_wrapper'>
         <div className = 'logo_section'>
-          <img src = '' alt = ''/>
+          <img src = 'https://brandlogos.net/wp-content/uploads/2016/06/linkedin-logo-512x512.png' alt = 'logo'/>
           <div className = 'search_bar'>
            <input placeholder = "search"></input>
            </div>
@@ -21,8 +22,8 @@ function Navbar() {
           <li><GroupIcon/>My Network</li>
           <li><WorkIcon/>Jobs</li>
           <li><TextsmsIcon/>Messaging</li>
-          <li>Notification</li>
-          <li>Me</li>
+          <li><NotificationsIcon/>Notification</li>
+          <li><img src = ''/> Me</li>
          </ul>
         </div>
 
