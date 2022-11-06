f731e16d13a4410c55b159a426f04143b6df8b35
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-06 20:14:09 +0000

    Working on the Main Page 🚀

---
+++ src/Components/Main Page/Maincontent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Maincontent.css'
+
+function Maincontent() {
+  return (
+    <div className = 'Main_content'>
+    Maincontent
+    </div>
+  )
+}
+
+export default Maincontent
