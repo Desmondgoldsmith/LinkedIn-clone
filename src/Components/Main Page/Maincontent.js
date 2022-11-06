712a6f526f92236807f3242252ec34e840aebf40
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-06 21:07:57 +0000

    Working on the Main Page 🚀

--- src/Components/Main Page/Maincontent.js
+++ src/Components/Main Page/Maincontent.js
@@ -6,6 +6,12 @@ function Maincontent() {
     <div className = 'Main_content'>
 
     <div className = 'left_content'>
+       <div className = 'backgroundImage' style = {{
+        backgroundImage : `url("https://media-exp1.licdn.com/dms/image/C4E16AQHAvKyuZ98BlQ/profile-displaybackgroundimage-shrink_350_1400/0/1662081045859?e=1673481600&v=beta&t=Bi_sMf1kHknAJ8m8oFPHQ414RwDV51_bMjWmsZkr0FU")`,
+        backgroundSize : 'cover',
+        backgroundRepeat : 'no-repeat',
+    }}>
+       </div>
         <h2>I'm on the left side</h2>
     </div>
     <div className = 'center_content'>
