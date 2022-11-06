edfa381ee6a370376e5b0eb9c0f70bc26c35e834
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-06 20:33:32 +0000

    Working on the Main Page 🚀

--- src/Components/Main Page/Maincontent.js
+++ src/Components/Main Page/Maincontent.js
@@ -4,14 +4,16 @@ import './Maincontent.css'
 function Maincontent() {
   return (
     <div className = 'Main_content'>
+
     <div className = 'left_content'>
-        
+        <h2>I'm on the left side</h2>
     </div>
     <div className = 'center_content'>
+    <h2>I'm in the center</h2>
 
     </div>
     <div className = 'right_content'>
-
+    <h2>I'm on the Right</h2>
     </div>
     </div>
   )
