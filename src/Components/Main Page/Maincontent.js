edba58f98a6ee05dd428fdd131dd9004426a14fa
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-09 23:36:02 +0000

    Working on the Main Page 🚀

--- src/Components/Main Page/Maincontent.js
+++ src/Components/Main Page/Maincontent.js
@@ -55,12 +55,8 @@ function Maincontent() {
       <p><GroupsIcon/><a href = "#">UX Design</a></p>
       <p><a href = "#">view all</a></p>
      </div>
-
-    <div className= "discover">
-       <h4>Discover</h4>
-    </div>
-
-    
+      <hr/>
+       <h4>Discover</h4>  
     </div>
 
 
