1ed09003e12668997b139698630c21069f04cd65
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-22 22:22:18 +0000

    Working on the Main Page 🚀

--- src/Components/Main Page/Options.js
+++ src/Components/Main Page/Options.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './options.css'
 import PanoramaIcon from '@mui/icons-material/Panorama';
 import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
-import WorkIcon from '@mui/icons-material/Work';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 function Options({name1,name2,name3,name4}) {
   return (
     <div className = 'options'>
      <PanoramaIcon className = "name1"/><p>{name1}</p>
      <SmartDisplayIcon className = "name2"/><p>{name2}</p>
-     <WorkIcon className = "name3"/><p>{name3}</p>
+     <BusinessCenterIcon className = "name3"/><p>{name3}</p>
      <NewspaperIcon className = "name4"/> <p>{name4}</p>
     </div>
   )
