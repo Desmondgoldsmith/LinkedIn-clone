3dbd04306c451e23e291cbdbf56f90c999d01fa5
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-22 21:56:33 +0000

    Working on the Main Page 🚀

--- src/Components/Main Page/Options.js
+++ src/Components/Main Page/Options.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PanoramaIcon from '@mui/icons-material/Panorama';
 import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
+import WorkIcon from '@mui/icons-material/Work';
 function Options() {
   return (
     <div className = 'options'>
      <PanoramaIcon/>
-     
+     <SmartDisplayIcon/>
     </div>
   )
 }
