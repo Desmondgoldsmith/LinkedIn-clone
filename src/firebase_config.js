f649eb94d2d4b0f894303b961fc7b408d50a06c8
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-11-23 00:19:58 +0000

    Working on the Main Page 🚀

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,2 +1,14 @@
 import {firebase} from 'firebase/compat/app';
 import {firestore} from 'firebase/compat/firestore'
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyDTQ5iiyzvVCOvJ0cQ-E_msL3_go8IYYUw",
+    authDomain: "linkedin-75990.firebaseapp.com",
+    projectId: "linkedin-75990",
+    storageBucket: "linkedin-75990.appspot.com",
+    messagingSenderId: "714034095063",
+    appId: "1:714034095063:web:bd5a8950ecac959d7ee192",
+    measurementId: "G-HN5SJHS7GF"
+})
+
+const DB = firebase.firestore(firebaseApp)
