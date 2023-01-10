a1edec57d3c209b9f8b1868bf47ba62fb9628b93
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-10 11:38:15 +0000

    working on the main page

--- src/Components/Main Page/Addpost.js
+++ src/Components/Main Page/Addpost.js
@@ -10,7 +10,7 @@ function Addpost({postImage,posts}) {
     <div className = 'post_container'>
        <div className = "post_header">
        <div className = "left_division">
-          <img src = "https://media-exp1.licdn.com/dms/image/C4D03AQEHVf4AElfMgg/profile-displayphoto-shrink_200_200/0/1614036542904?e=1673481600&v=beta&t=WP0DfeTq4YdzmsnOyL0JoBRNZt5WKYHP1s9DpYDXboY" alt = "profile_image"/>
+          <img src = "https://media.licdn.com/dms/image/D4E03AQGwZt1JT4mh4g/profile-displayphoto-shrink_200_200/0/1673142753502?e=1678924800&v=beta&t=N-mjxLo38JjH-E9kZC9bXXLhOqTFLcSNuIw3dBc85wk" alt = "profile_image"/>
        </div>
        <div className = 'right_division'>
         <h3>Desmond Goldsmith</h3>
@@ -25,7 +25,7 @@ function Addpost({postImage,posts}) {
        </div>
        <div className='reactions'>
         <div className='left_reaction'>
-         <img src = 'https://media.licdn.com/dms/image/D4E03AQGwZt1JT4mh4g/profile-displayphoto-shrink_200_200/0/1673142753502?e=1678924800&v=beta&t=N-mjxLo38JjH-E9kZC9bXXLhOqTFLcSNuIw3dBc85wk' alt='like'/>
+         <img src = 'https://static-exp1.licdn.com/sc/h/8ekq8gho1ruaf8i7f86vd1ftt' alt='like'/>
          <p>You liked this.</p>
         </div>
         <div className = 'right_reaction'>
