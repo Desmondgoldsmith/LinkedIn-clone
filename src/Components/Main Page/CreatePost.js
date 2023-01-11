59a4c024a6238206bce341505e546ad667c3c80a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-11 13:51:57 +0000

    working on the creat Post form

--- src/Components/Main Page/CreatePost.js
+++ src/Components/Main Page/CreatePost.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function CreatePost() {
+    const [text,setText] = useState('')
+    
+    const savePost = (e) =>{
+
+    }
+
   return (
     <div className = "wrapper">
        <div className='container'>
-
+       <form onSubmit={(e)=>savePost(e)}>
+        <textarea type="text" value={text} onChange = {(e)=>setText(e.target.value)} name = "name" placeholder = "start a post" />
+        <button type = "submit">post</button>
+       </form>
        </div>
     </div>
   )
