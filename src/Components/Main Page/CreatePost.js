a62a71a7c37eba406a3917ca7658643ae4de1554
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-11 15:01:54 +0000

    working on the creat Post form

--- src/Components/Main Page/CreatePost.js
+++ src/Components/Main Page/CreatePost.js
@@ -11,12 +11,14 @@ function CreatePost() {
 
   return (
     <div className = "wrapper fixed top-0 left-0 right-0 z-50  w-[100%] h-full justify-start ">
-       <div className='container bg-white w-[40%] mt-[40px]  m-auto flex  p-[2rem] rounded-[8px] relative'>
-        <div className="head flex items-center justify-between">
-            <div><p>Create a Post</p></div>
+       <div className='container bg-white w-[40%] mt-[40px]  m-auto flex  p-[10px] rounded-[8px] relative'>
+        <div className="head flex items-center space-x-96">
+            <div className='text-[20px]'><p>Create a Post</p></div>
             <div><CloseIcon/></div>
         </div>
         <hr className='mt-3 mb-3 '/>
+
+        
           
        </div>
     </div>
