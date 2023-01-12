d699f93531a5bb51fc74c3e1d2b0388767bc0748
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-12 17:06:28 +0000

    working on the creat Post form

--- src/Components/Main Page/CreatePost.js
+++ src/Components/Main Page/CreatePost.js
@@ -88,7 +88,7 @@ function CreatePost({formx,setForm}) {
          {image ?
          <div className='p-3'>
          <div className='hover:rounded-full hover:bg-gray-300 p-1 cursor-pointer float-right' onClick = {()=>closeForm()}><CloseIcon/></div>
-           <img src={image} alt = "selected image" className="w-[98%] p-3 h-[40%]"/>
+           <img src={image} alt = "selected image" className="w-[98%] p-3 h-[30%]"/>
          
          </div>
          :
