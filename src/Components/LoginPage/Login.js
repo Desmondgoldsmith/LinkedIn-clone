b402efa01e75a564c6185a0e7751fec641874bae
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-18 18:44:12 +0000

    working on the creat Post form

--- src/Components/LoginPage/Login.js
+++ src/Components/LoginPage/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Login() {
   return (
     <div>
-      <div className = "p-4 ml-20">
+      <div className = "p-4 ml-[150px]">
         {/* logo */}
         <img className='w-12' src = "https://www.seekpng.com/png/full/8-84419_linkedin-logo-png-icon-linkedin-logo-png.png" alt = "logo"/>
       </div>
