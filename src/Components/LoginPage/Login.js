cf1d2208bed662609c02f94fdde8e373db380fd6
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-18 18:34:44 +0000

    working on the creat Post form

--- src/Components/LoginPage/Login.js
+++ src/Components/LoginPage/Login.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Login() {
   return (
     <div>
-      
+      <div className = "">
+        {/* logo */}
+        <img src = "https://www.seekpng.com/png/full/8-84419_linkedin-logo-png-icon-linkedin-logo-png.png" alt = "logo"/>
+      </div>
     </div>
   )
 }
