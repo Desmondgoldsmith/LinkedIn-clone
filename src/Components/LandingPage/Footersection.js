014f7eaf35209f2a400f251f476a8c5aa3c3f23b
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-20 09:32:26 +0000

    working on the Login Form

--- src/Components/LandingPage/Footersection.js
+++ src/Components/LandingPage/Footersection.js
@@ -1,20 +1,19 @@
 import React from 'react'
-// import './footersection.css'
+import './footersection.css'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 function Footersection(){
     return (
-        <div className="footer-section bg-white flex">
-         <div className = "left_ flex space-x-1">
-          <img className='w-[100px] ' src = 'https://logosvector.net/wp-content/uploads/2015/09/linkedin-black-logo.png'/>
-         <p>© 2022</p>
+        <div className="footer-section">
+         <div className = "left_">
+         <p className='logo'><img src = 'https://logosvector.net/wp-content/uploads/2015/09/linkedin-black-logo.png'/> © 2022</p>
          <p><a href = "#">About</a></p>
          <p><a href = "#">Accessibility</a> </p>
         <p><a href = "#">User Agreement</a> </p>
         <p><a href = "#">Privacy Policy</a></p>
         <p><a href = "#">Cookie Policy</a></p>
          </div>
-         <div className="right flex">
+         <div className="right">
         <p><a href = "#">Copyright Policy</a></p>
         <p><a href = "#">Brand Policy</a></p>
         <p><a href = "#">Guest Controls</a></p>
