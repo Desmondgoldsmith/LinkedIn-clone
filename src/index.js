389cc37e10c16420ca55fa927ea843672734c4fa
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-25 12:04:23 +0000

    working on the Login Functionality with Redux

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import {Provider} from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <Provider>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
