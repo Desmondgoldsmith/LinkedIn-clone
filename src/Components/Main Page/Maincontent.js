9160979113b296b62f55623f3e3474c1e394389c
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-30 11:52:06 +0000

    working on the Login Functionality with Redux

--- src/Components/Main Page/Maincontent.js
+++ src/Components/Main Page/Maincontent.js
@@ -151,8 +151,8 @@ const displayForm = (e) => {
     </div>
 
     {/* message section */}
-    <div className = "w-[300px] bg-white border border-red-600 z-15  ">
-      <div className = " inset-x-0 sticky bottom-0 h-16">
+    <div className = "w-[300px] bg-white border border-red-600 z-15 bottom-0 ">
+      <div className = "sticky bottom-0 h-16">
               <h2>Desssssss</h2>
       </div>
     </div>
