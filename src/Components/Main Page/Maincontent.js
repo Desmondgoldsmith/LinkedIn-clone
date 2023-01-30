c56cd4dd6651c6979b19a6860fbc1dd7cfc9ee4e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-30 11:42:41 +0000

    working on the Login Functionality with Redux

--- src/Components/Main Page/Maincontent.js
+++ src/Components/Main Page/Maincontent.js
@@ -151,7 +151,7 @@ const displayForm = (e) => {
     </div>
 
     {/* message section */}
-    <div className = "w-[200px] bg-white border border-red-600">
+    <div className = "w-[300px] bg-white border border-red-600 z-15">
       <h2>Desssssss</h2>
     </div>
     
