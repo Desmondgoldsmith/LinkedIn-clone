ab1550d49eb08b50b51e6d37bbc247c10b337075
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2023-01-30 12:24:08 +0000

    working on the Login Functionality with Redux

--- src/Components/Main Page/Maincontent.js
+++ src/Components/Main Page/Maincontent.js
@@ -64,7 +64,7 @@ const displayForm = (e) => {
    <CreatePost formx = {formx} setForm = {setForm}/>
    
    {/* message section */}
-   <div className = "w-[350px] bg-white z-50 fixed bottom-0 right-0 h-15 rounded-sm">
+   <div className = "w-[350px] bg-white z-50 fixed bottom-0 right-0 h-[20px] rounded-md">
         <h2>Desssssss</h2>
     </div>
 
